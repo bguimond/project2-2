d7299a1709a7a4486c970d7738fa64c9c32105e6
Autograde Server <autograde@autograde.local>
2021-03-05 09:15:57 +0000

    Initial Starter Generated

---
+++ rectangle-coverage.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+;; Project 2: Computing the total area covered by a set of rectangles (Quad-trees and immutable data-structures)
+
+(provide total-rect-area)
+
+; total-rect-area: returns the total integer area covered by any (one or more) rectangles in the given list 
+; Don't double-count. Should be identical to the same solution from e3.rkt, except this version must be in O(n log n)
+; Hint: try implementing an immutable quad-tree to represent 2D space; FYI, a solution takes only ~30-40 lines of code.
+(define (total-rect-area rect-list)
+  'todo)
+
